0bfe8592aad78d971825aa474c6e97f19843494b
CHO HYE RA <s2037@e-mirim.hs.kr>
2021-09-11 23:34:34 +0900

    스플래시 화면 구현

--- app/src/main/java/com/example/medo/ChallengeActivity.java
+++ app/src/main/java/com/example/medo/ChallengeActivity.java
@@ -3,17 +3,12 @@ package com.example.medo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button btnJoin;
+public class ChallengeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_challenge);
     }
 }
