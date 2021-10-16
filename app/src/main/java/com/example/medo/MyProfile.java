fd55a5f9c03edb7b4730da7327790ebc5984b63e
[haezzang] <songna89@naver.com>
2021-10-16 20:48:16 +0900

    myprofile 생성

---
+++ app/src/main/java/com/example/medo/MyProfile.java
@@ -0,0 +1,19 @@
+package com.example.medo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MyProfile extends AppCompatActivity {
+
+    Button btnJoin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myprofile);
+
+
+    }
+}
