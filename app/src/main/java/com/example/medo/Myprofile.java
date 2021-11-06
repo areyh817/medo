904b8632d82daa4d0a90e78e2eb1d746e23e5950
CHO HYE RA <s2037@e-mirim.hs.kr>
2021-11-06 23:15:31 +0900

    로그인 성공시 화면 전환 / Firebase 실시간 데이터 사용

--- app/src/main/java/com/example/medo/Myprofile.java
+++ app/src/main/java/com/example/medo/Myprofile.java
@@ -1,24 +1,36 @@
 package com.example.medo;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Myprofile extends Fragment {
     Menu activity;
-
-
+    TextView userId;
+    FirebaseDatabase db;
+    DatabaseReference ref;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_myprofile , container, false);
 
-
-
         return rootView;
 
-
     }
+
+
 }
