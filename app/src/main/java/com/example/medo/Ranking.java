697f55272e30c435fedb992b389739ea58f241a0
[haezzang] <songna89@naver.com>
2021-11-09 23:06:16 +0900

    랭킹 리스트뷰 구현

--- app/src/main/java/com/example/medo/Ranking.java
+++ app/src/main/java/com/example/medo/Ranking.java
@@ -1,24 +1,62 @@
 package com.example.medo;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+
+import java.util.ArrayList;
+
 public class Ranking extends Fragment {
-    Menu activity;
+
+    ArrayList<Rank> ranks;
+    ListView listview_rank;
+    private static CustomAdapter_ranking customAdapter;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
 
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_ranking , container, false);
+        View rootView = inflater.inflate(R.layout.fragment_ranking, container, false);
 
+
+        //기본 데이터
+        ranks = new ArrayList<>();
+        ranks.add(new Rank("조조"));
+
+
+
+        listview_rank = (ListView) rootView.findViewById(R.id.listview_rank);
+        customAdapter = new CustomAdapter_ranking(getContext(),ranks);
+        listview_rank.setAdapter(customAdapter);
 
 
         return rootView;
-
-
     }
+}
+
+
+//data class
+class Rank {
+    private String name;
+    public Rank(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+
 }
